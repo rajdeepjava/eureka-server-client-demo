ccf94cabf9fd3c6785dbdc2d5e8f681e3476c31a
rajdeep <rajdeep.gautam@gmail.com>
2021-09-10 14:52:02 +0530

    "second commit for eureka service"

---
+++ EurekaService-1/src/test/java/com/eureka/service1/EurekaService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
